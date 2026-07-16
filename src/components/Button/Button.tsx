ca7ba1d577a3091e99febc54438e8390d5d71ce3
Makuachukwu Iloabachie <iloabachiem@gmail.com>
2026-07-16 11:46:24 +0100

    create reusuable button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,14 @@
-import type { ReactNode } from "react";
+import type { ButtonHTMLAttributes, ReactNode } from "react";
 
-type ButtonProps = {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
-  onClick?: () => void;
 };
 
-function Button({ children, onClick }: ButtonProps) {
+function Button({ children, className = "", ...props }: ButtonProps) {
   return (
     <button
-      onClick={onClick}
-      className="rounded-lg bg-[#3366FF] px-4 py-3 text-white font-medium"
+      className={`rounded-lg bg-[#3366FF] px-4 py-3 text-white font-medium ${className}`}
+      {...props}
     >
       {children}
     </button>
