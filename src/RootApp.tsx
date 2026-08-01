95176e2add97ea72d8e274d12346bea743bc6624
Makuachukwu Iloabachie <iloabachiem@gmail.com>
2026-08-01 23:28:15 +0100

    mobile view fix

--- src/RootApp.tsx
+++ src/RootApp.tsx
@@ -1,16 +1,9 @@
-import MobileApp from './App';
 import DesktopApp from './web/App';
-import { useIsDesktop } from './hooks/useIsDesktop';
 
 export default function RootApp() {
-  const isDesktop = useIsDesktop();
-  return isDesktop ? (
+  return (
     <div className="web-root">
       <DesktopApp />
-    </div>
-  ) : (
-    <div className="mobile-root">
-      <MobileApp />
     </div>
   );
 }
